f790a868fe1c2523fdf40a170f7c0f9adf396dff
Roman Gille <developer@romangille.com>
2020-03-24 22:48:53 +0100

    initial commit

---
+++ local-heroes.py
@@ -0,0 +1,79 @@
+import urllib.request
+import json
+import time
+
+GMapsApiKey = ''
+wrapApiKey = ''
+apiUrl = 'https://wrapapi.com/use/r-dent/side-projects/local-hero/latest?wrapAPIKey=' + wrapApiKey
+
+def findLocationGMaps(locationName):
+    geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?key=' + GMapsApiKey + '&address=' + urllib.parse.quote(locationName)
+    
+    print("Loading Location for %s." % locationName)
+
+    with urllib.request.urlopen(geocodeUrl) as response:
+        content = response.read()
+        results = json.loads(content)['results']
+
+        if len(results) > 0:
+            firstResult = results[0]
+
+            latitute = firstResult['geometry']['location']['lat']
+            longitude = firstResult['geometry']['location']['lng']
+            location = [latitute, longitude]
+
+            print('Found %s.' % location)
+
+        else:
+            location = None
+            print('NOT FOUND')
+
+        return location
+
+def findLocationOSM(locationName):
+    geocodeUrl = "https://nominatim.openstreetmap.org/search?format=json&q="+ urllib.parse.quote(locationName)
+    
+    print("Loading Location for %s." % locationName)
+
+    with urllib.request.urlopen(geocodeUrl) as response:
+        content = response.read()
+        results = json.loads(content)
+
+        if len(results) > 0:
+            firstResult = results[0]
+
+            latitute = firstResult['lat']
+            longitude = firstResult['lon']
+            location = [latitute, longitude]
+
+            print('Found %s.' % location)
+
+        else:
+            location = None
+            print('NOT FOUND')
+
+        return location
+
+def loadLocals():
+    with urllib.request.urlopen(apiUrl) as response:
+        content = response.read()
+        parsed_json = json.loads(content)
+        entries = parsed_json['data']['output']
+        titles = []
+        localHeroes = []
+
+        for entry in entries:
+            title = entry['title'].split('/ ')[0].strip()
+
+            if title not in titles:
+                titles.append(title)
+                entry["cleanTitle"] = title
+                entry["location"] = findLocationGMaps(title + " Leipzig")
+                localHeroes.append(entry)
+                time.sleep(1)
+
+        print(localHeroes)
+
+# loadLocals()
+print(findLocationGMaps('Kleine Leckerei Leipzig'))
+print(findLocationOSM('Kleine Leckerei Leipzig'))
