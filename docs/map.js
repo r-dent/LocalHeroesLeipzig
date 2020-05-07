1f5a0d15ae6f9904f701e0bdd0d2abf9873ca422
Roman Gille <developer@romangille.com>
2020-05-07 14:26:03 +0200

    update resource version

--- docs/map.js
+++ docs/map.js
@@ -40,7 +40,7 @@ class LocalHeroesMap {
         mapContainer.classList.add('lh-mp-ctnr')
         mapContainer.innerHTML = '<div id="loading"><svg height="100" width="100" class="spinner"><circle cx="50" cy="50" r="20" class="inner-circle" /></svg></div>'
         
-        const resourceVersionTag = '20200506'
+        const resourceVersionTag = '20200507'
         const dataUrl = (this.isLocal ? '../' : this.repositoryBaseUrl) +'data/local-heroes-leipzig.geojson?v='+ resourceVersionTag
         const cssUrl = (this.isLocal ? '' : this.repositoryBaseUrl +'docs/') +'map-style.css?v='+ resourceVersionTag
 
